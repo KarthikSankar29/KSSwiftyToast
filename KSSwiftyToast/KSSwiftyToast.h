c5347ac144dc487dafffcc4380933237c6cd4b88
Karthik Sankar <skarthikgac@gmail.com>
2016-09-25 17:13:35 +0530

    Initial Commit

---
+++ KSSwiftyToast/KSSwiftyToast.h
@@ -0,0 +1,17 @@
+//
+//  KSSwiftyToast.h
+//  KSSwiftyToast
+//
+//  Created by Karthik S on 25/09/16.
+//  Copyright © 2016 KarthikSankar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for KSSwiftyToast.
+FOUNDATION_EXPORT double KSSwiftyToastVersionNumber;
+
+//! Project version string for KSSwiftyToast.
+FOUNDATION_EXPORT const unsigned char KSSwiftyToastVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <KSSwiftyToast/PublicHeader.h>
